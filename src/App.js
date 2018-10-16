b14cb4a8035aa16d0f02cf4889f69f38e80d7470
daniel <seahagos@gmail.com>
2018-10-15 23:42:47 -0700

    provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
-import Main from "./components/Main/Main";
+import { Provider } from "react-redux";
+// import Main from "./components/Main/Main";
 import { HashRouter } from "react-router-dom";
 import Nav from "./components/Header/Nav";
 import routes from "./routes";
+import store from "./store";
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <div className="App">
-          <Nav />
-          {/* <Main /> */}
-          {routes}
-        </div>
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <div className="App">
+            <Nav />
+            {/* <Main /> */}
+            {routes}
+          </div>
+        </HashRouter>
+      </Provider>
     );
   }
 }
