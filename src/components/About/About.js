2bc78ae28a0a556545b3c227ba63a5127cfe18f9
daniel <seahagos@gmail.com>
2018-10-16 23:43:52 -0700

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class About extends Component {
   render() {
-    return(
-      <div><h1>About Page</h1></div>
-    )
+    return (
+      <div>
+        <p>
+          Highly Motivated and Passionate Software engineer with thorough
+          hands-on experience in all levels of the development lifecycle. Strong
+          background in object-oriented programming, designing interfaces,
+          developing, testing, debugging and deployment. High-flier in both team
+          and self-directed settings. With strong collaborative, interpersonal,
+          leadership and communication skills.{" "}
+        </p>
+      </div>
+    );
   }
 }
 
