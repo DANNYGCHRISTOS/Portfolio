ddef1ba1c294d384aeae6f14b5dfc811f8706147
daniel <seahagos@gmail.com>
2018-10-16 23:45:44 -0700

    experience

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+class Experience extends Component {
+    state = {  }
+    render() { 
+        return ( <div>Experience</div> );
+    }
+}
+ 
+export default Experience;
