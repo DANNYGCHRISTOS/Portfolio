6948b411ff9039760b991d607ddc04c3c7695ee1
daniel <seahagos@gmail.com>
2018-10-18 01:01:32 -0700

    clip path

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <HashRouter>
         <div className="App">
           <Header />
-          {/* <Main /> */}
+          <Home/>
           {routes}
         </div>
       </HashRouter>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./Home.css";
 class Main extends Component {
   render() {
-    return <div>Main Page</div>;
+    return <div className="container">Main Page</div>;
   }
 }
 
